f3c8f8b8c396b9a7c76c6b1adf3b302ff589fff4
Ana Durand <ana.dc1004@gmail.com>
2017-07-14 15:42:34 -0500

    mas estilos

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,6 +53,11 @@ $(document).ready(function(){
       $('.close').on('click',(e)=>{
         $('#slide-out').sideNav('hide');
       });
-
+      $('.link-scroll').on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 800, 'linear');
+        $('#slide-out').sideNav('hide');
+        
+    });
 
 });
